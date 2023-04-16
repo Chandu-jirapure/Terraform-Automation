e96f34f44d9e18fdb6a737468f2a310e1143fd23
Govind Jadhav <52379705+govindjadhav@users.noreply.github.com>
2023-04-16 19:00:42 +0530

    hello

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "chandrakantjirapure-3"
+    bucket = "shraddha-terraform-pathare"
     key = "main"
     region = "ap-south-1"
     dynamodb_table = "my-dynamo-DB"
